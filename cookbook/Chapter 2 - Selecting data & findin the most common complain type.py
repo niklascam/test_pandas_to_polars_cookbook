43c8023246b87893bb6719b4b0b20407e07ee78b
Niklas Schmitz <ns751@cam.ac.uk>
2024-10-26 08:04:01 +0100

    chapter_2

--- cookbook/Chapter 2 - Selecting data & findin the most common complain type.py
+++ cookbook/Chapter 2 - Selecting data & findin the most common complain type.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import polars as pl
 
+DATA_PATH = "../data/311-service-requests.csv"
 # %%
 # We're going to use a new dataset here, to demonstrate how to deal with larger datasets. This is a subset of the of 311 service requests from [NYC Open Data](https://nycopendata.socrata.com/Social-Services/311-Service-Requests-from-2010-to-Present/erm2-nwe9).
 # because of mixed types we specify dtype to prevent any errors
@@ -14,20 +15,21 @@ complaints.head()
 # Hint: we need the dtype argument reading all columns in as strings above in Pandas due to the zip code column containing NaNs as "NA" and some zip codes containing a dash like 1234-456
 # you cannot exactly do the same in Polars but you can read about some other solutions here:
 # see a discussion about dtype argument here: https://github.com/pola-rs/polars/issues/8230
-
+pl_complaints = pl.read_csv(DATA_PATH, infer_schema_length=0)
 # %%
 # Selecting columns:
 complaints["Complaint Type"]
 
 # %%
 # TODO: rewrite the above using the polars library
-
+pl_complaints.select(pl.col("Complaint Type"))
 # %%
 # Get the first 5 rows of a dataframe
 complaints[:5]
 
 # %%
 # TODO: rewrite the above using the polars library
+pl_complaints.head(5)
 
 # %%
 # Combine these to get the first 5 rows of a column:
@@ -35,7 +37,7 @@ complaints["Complaint Type"][:5]
 
 # %%
 # TODO: rewrite the above using the polars library
-
+pl_complaints.select(pl.col("Complaint Type")).head(5)
 
 # %%
 # Selecting multiple columns
@@ -43,6 +45,9 @@ complaints[["Complaint Type", "Borough"]]
 
 # %%
 # TODO: rewrite the above using the polars library
+pl_complaints.select([pl.col("Complaint Type"), pl.col("Borough")])
+#or just:
+pl_complaints.select(["Complaint Type", "Borough"])
 
 # %%
 # What's the most common complaint type?
@@ -51,6 +56,13 @@ complaint_counts[:10]
 
 # %%
 # TODO: rewrite the above using the polars library
+pl_most_complaints = (
+    pl_complaints.select(pl.col("Complaint Type"))
+    .to_series()
+    .value_counts()
+    .sort("count",descending=True)
+    .head(10)
+)
 
 # %%
 # Plot the top 10 most common complaints
@@ -64,3 +76,4 @@ plt.show()
 
 # %%
 # TODO: please do the same with Polars
+pl_most_complaints.plot.bar(x="Complaint Type", y="count")
