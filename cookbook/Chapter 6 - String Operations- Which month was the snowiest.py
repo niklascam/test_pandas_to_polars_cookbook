ed6a49b0ee267fcbda581e8c918605a5ede299cb
Niklas Schmitz <ns751@cam.ac.uk>
2024-10-26 10:28:03 +0100

    chapter 6 to 8 done

--- cookbook/Chapter 6 - String Operations- Which month was the snowiest.py
+++ cookbook/Chapter 6 - String Operations- Which month was the snowiest.py
@@ -2,12 +2,14 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import polars as pl
+import seaborn as sns
 
 plt.style.use("ggplot")
 plt.rcParams["figure.figsize"] = (15, 3)
 plt.rcParams["font.family"] = "sans-serif"
 
-
+DATA_PATH= "../data/weather_2012.csv"
 # %%
 # We saw earlier that pandas is really good at dealing with dates. It is also amazing with strings! We're going to go back to our weather data from Chapter 5, here.
 weather_2012 = pd.read_csv(
@@ -16,7 +18,7 @@ weather_2012 = pd.read_csv(
 weather_2012[:5]
 
 # TODO: load the data using polars and call the data frame pl_wather_2012
-
+pl_weather_2012 = pl.read_csv(DATA_PATH, try_parse_dates=True)
 
 # %%
 # You'll see that the 'Weather' column has a text description of the weather that was going on each hour. We'll assume it's snowing if the text description contains "Snow".
@@ -30,8 +32,11 @@ is_snowing.plot()
 plt.show()
 
 # TODO: do the same with polars
-
-
+pl_weather_description = pl_weather_2012.with_columns(
+    is_snowing=pl.col("weather").str.contains("Snow")
+)
+sns.barplot(pl_weather_description.to_pandas(), x="date_time", y="is_snowing")
+# or: sns.barplot(pl_weather_description.to_pandas(), x="date_time", y="is_snowing")  # requires pyarrow
 # %%
 # If we wanted the median temperature each month, we could use the `resample()` method like this:
 weather_2012["temperature_c"].resample("M").apply(np.median).plot(kind="bar")
@@ -40,7 +45,12 @@ plt.show()
 # Unsurprisingly, July and August are the warmest.
 
 # TODO: and now in Polars
+pl_weather_2012_monthly = pl_weather_2012.group_by_dynamic(
+    "date_time", every="1mo"
+).agg(pl.col("temperature_c").median())
+pl_weather_2012_monthly.head()
 
+pl_weather_2012_monthly.plot.bar(x="date_time", y="temperature_c")
 
 # %%
 # So we can think of snowiness as being a bunch of 1s and 0s instead of `True`s and `False`s:
@@ -53,3 +63,9 @@ plt.show()
 # So now we know! In 2012, December was the snowiest month. Also, this graph suggests something that I feel -- it starts snowing pretty abruptly in November, and then tapers off slowly and takes a long time to stop, with the last snow usually being in April or May.
 
 # TODO: please do the same in Polars
+pl_weather_description = pl_weather_description.group_by_dynamic(
+    "date_time", every="1mo"
+).agg(pl.col("is_snowing").mean())
+pl_weather_description.head()
+
+pl_weather_description.plot.bar(x="date_time", y="is_snowing")
--- cookbook/Chapter 8 - How to deal with timestamps.py
+++ cookbook/Chapter 8 - How to deal with timestamps.py
@@ -1,5 +1,9 @@
 # %%
+from os import sep
 import pandas as pd
+import polars as pl
+
+DATA_PATH = "../data/popularity-contest"
 
 # %%
 # Parsing Unix timestamps
@@ -14,7 +18,18 @@ popcon.columns = ["atime", "ctime", "package-name", "mru-program", "tag"]
 popcon[:5]
 
 # TODO: please reimplement this using Polars
+pl_popcon = pl.read_csv(DATA_PATH, separator=" ", infer_schema=0)[:-1]
+# NOTE: or you can do this: 
+nr_rows = (
+    pl.scan_csv(DATA_PATH, separator=" ", infer_schema=0)
+    .select(pl.len())
+    .collect()
+    .item()
+)
+pl_popcon = pl.read_csv(DATA_PATH, separator=" ", infer_schema=0, n_rows=(nr_rows - 1))
 
+pl_popcon.columns = ["atime", "ctime", "package-name", "mru-program", "tag"]
+pl_popcon.head(5)
 
 # %%
 # The magical part about parsing timestamps in pandas is that numpy datetimes are already stored as Unix timestamps. So all we need to do is tell pandas that these integers are actually datetimes -- it doesn't need to do any conversion at all.
@@ -23,7 +38,10 @@ popcon["atime"] = popcon["atime"].astype(int)
 popcon["ctime"] = popcon["ctime"].astype(int)
 
 # TODO: please reimplement this using Polars
-
+pl_popcon = pl_popcon.with_columns(
+    pl.col("atime").cast(pl.Int64),
+    pl.col("ctime").cast(pl.Int64),
+)
 
 # %%
 # Every numpy array and pandas series has a dtype -- this is usually `int64`, `float64`, or `object`. Some of the time types available are `datetime64[s]`, `datetime64[ms]`, and `datetime64[us]`. There are also `timedelta` types, similarly.
@@ -34,7 +52,15 @@ popcon["ctime"] = pd.to_datetime(popcon["ctime"], unit="s")
 popcon.head()
 
 # TODO: please reimplement this using Polars
-
+# convert to milliseconds
+pl_popcon = pl_popcon.with_columns(
+    pl.col("atime") * 1000,
+    pl.col("ctime") * 1000,
+)
+pl_popcon = pl_popcon.with_columns(
+    pl.col("atime").cast(pl.Datetime("ms")),
+    pl.col("ctime").cast(pl.Datetime("ms")),
+)
 
 # %%
 # Now suppose we want to look at all packages that aren't libraries.
@@ -47,6 +73,7 @@ nonlibraries = popcon[~popcon["package-name"].str.contains("lib")]
 nonlibraries.sort_values("ctime", ascending=False)[:10]
 
 # TODO: please reimplement this using Polars
-
+pl_popcon = pl_popcon.filter(pl.col("atime") > pl.datetime(year=1970, month=1, day=1))
+pl_nonlibraries = pl_popcon.filter(~pl.col("package-name").str.contains("lib"))
 
 # The whole message here is that if you have a timestamp in seconds or milliseconds or nanoseconds, then you can just "cast" it to a `'datetime64[the-right-thing]'` and pandas/numpy will take care of the rest.
